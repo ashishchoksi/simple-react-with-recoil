1ebc7c824747f2a39c78fe19490d1eef6b7ca5b9
ashish <ashishchoksi84@gmail.com>
2023-07-15 13:47:28 +0530

    Add counter state and see the problem of re-rendering and props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from '@mui/material/Card';
 import { Button, Typography } from '@mui/material';
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
       <Card style={{width: '400px', padding: '20px'}}>
         <Typography style={{display: 'flex', justifyContent: 'center'}} variant='h6'>Simple react count App</Typography>
-        <Buttons/>
-        <CounterComponent />
+        <Buttons count={count} setCount={setCount}/>
+        <CounterComponent count={count} />
       </Card>
     </div>
   )
 }
 
-function CounterComponent() {
+function CounterComponent({count}) {
   return <div style={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
-    0
+    {count}
   </div>
 }
 
-function Buttons() {
+function Buttons({count, setCount}) {
   return <div style={{display: 'flex', justifyContent: 'space-between', marginTop: 10}}>
-    <Increate />
-    <Decrease />
+    <Increate count={count} setCount={setCount} />
+    <Decrement count={count} setCount={setCount} />
   </div>
 }
 
-function Increate() {
-  return <Button variant="contained">Increment</Button>
+function Increate({count, setCount}) {
+  return <Button variant="contained" onClick={() => setCount(count + 1)}>Increment</Button>
 }
 
-function Decrease() {
-  return <Button variant="contained">Increment</Button>
+function Decrement({count, setCount}) {
+  return <Button variant="contained" onClick={() => setCount(count - 1)}>Decrement</Button>
 }
 
 export default App
